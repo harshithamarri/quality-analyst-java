97bb06b192da59fe98f2e8de0ccc923344db2621
harshithamarri <marriharshitha99@gmail.com>
2017-05-30 22:05:09 -0500

    Constructor

--- Employee.java
+++ Employee.java
@@ -1,38 +1,36 @@
+package constructor;
 
 public class Employee {
-    public int empId;
-    public String empName;
-    public  static String companyName;
+	
+	public int empId;
+	public String empName;
+	
+	public Employee()//default constructor
+	{
+		System.out.println("I am in default constructor");
+	}
+	
+	public Employee(int id, String name)
+	{
+		empId = id;
+		empName = name;
+	}
+
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee.companyName = "Infosys";
 		
-		Employee Ramu = new Employee();
+		Employee Ramu=new Employee();
 		Ramu.empId = 1000;
 		Ramu.empName = "Ramu";
-		//Ramu.companyName = "Infosys";
+		System.out.println(Ramu.empId);
+		System.out.println(Ramu.empName);
 		
-		System.out.println(Ramu.empId );
-		System.out.println(Ramu.empName );
-		System.out.println(Employee.companyName);
+		Employee Raju=new Employee(2000,"Raju");
+		System.out.println(Raju.empId);
+		System.out.println(Raju.empName);
 		
-		
-		Employee Raju = new Employee();
-		Raju.empId = 2000;
-		Raju.empName = "Raju";
-		//Raju.companyName = "Infosys";
-		
-		System.out.println(Raju.empId );
-		System.out.println(Raju.empName );
-		System.out.println(Employee.companyName);
-		
-		Employee Rakesh = new Employee();
-		
-		System.out.println(Rakesh.empId );
-		System.out.println(Rakesh.empName );
-		System.out.println(Employee.companyName);
 		
 	}
 
