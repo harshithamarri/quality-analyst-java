c19a06a776b7b371f6951ddfff95582ad61a8b60
harshithamarri <marriharshitha99@gmail.com>
2017-05-30 22:08:18 -0500

    methodoverloading

--- Operations.java
+++ Operations.java
@@ -1,39 +1,36 @@
-package exceptionhandling;
+package methodoverloading;
 
 public class Operations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int x = 100;
-		int y = 10;
-		
-		try{
-		int z = x/y;
-		
-		System.out.println(z);
-		}
-		
-		catch(Exception e)
-		{
-			System.out.println("Exception handled");
-			System.out.println(e);
-		 System.out.println(e.getMessage());
-		
-		}
-		
-		finally
-		{
-			System.out.println("ALways executes");
-		}
-		
-		
-		System.out.println("Exception occured");
-	
-		
-		
-		
 
+		Operations obj=new Operations();
+		obj.add(100, 200);
+		obj.add(100,300,400);
+		obj.add(100, 200.123);
+		obj.add(100.123, 200);
+	}
+	
+	//compile time polymorphism or static polymorphism
+	public void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+
+	public void add(int a, int b, int c)
+	{
+		System.out.println(a+b+c);
+	}
+	
+	public void add(int a, double b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public void add(double a, int b)
+	{
+		System.out.println(a+b);
 	}
 
 }
